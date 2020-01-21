fbe25e5e8990807ba324a59a29acc02020b41141
marvermar <m.veramartin@gmail.com>
2020-01-21 18:32:34 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../stylesheets/App.css';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return <Header />
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="menu">
+                <ul>
+                    <li>Welcome</li>
+                    <li>About me</li>
+                    <li>Works</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <image src="../stylesheets/images/Dollify-c11a2fa7-8f10-4ae6-ada0-409bbc9c4b26-3.png" alt="logo"></image>
+        </header>
+    )
+}
+
+export default Header;
