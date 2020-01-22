6fa749336bf3d229da5c1520d24f6d7c662ff9d9
marvermar <m.veramartin@gmail.com>
2020-01-22 14:52:26 +0100

    create welcome component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './stylesheets/App.scss'
 import Header from './components/Header';
+import Welcome from './components/Welcome';
 
 class App extends React.Component {
   render() {
-    return <Header />
+    return <div>
+      <Header />
+      <Welcome />
+    </div>
   }
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <section className="welcome">
+            <h1 className="welcome_title">Hello.</h1>
+            <h2 className="welcome_name"> I'm María, front-end developer. </h2>
+            <p className="welcome_text"> My mind ,that has thirst for knowledge, is always active looking for new challenges and ways to see the world</p>
+        </section>
+    )
+}
+export default Welcome;
