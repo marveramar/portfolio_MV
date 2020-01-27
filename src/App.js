282b9de2fa59626d8ce56ddabe6bdbc216d31ed6
marvermar <m.veramartin@gmail.com>
2020-01-27 21:56:38 +0100

    start component about me, create text and some styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './stylesheets/App.scss'
 import Header from './components/Header';
 import Welcome from './components/Welcome';
+import AboutMe from './components/AboutMe';
 
 class App extends React.Component {
   render() {
     return <div>
       <Header />
       <Welcome />
+      <AboutMe />
     </div>
   }
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return <section className="aboutMe">
+        <div className="aboutMe_img">
+            <img src="../stylesheets/images/dolly.png" alt="fotografía de la autora" />
+        </div>
+        <div className="aboutMe_description">
+            <p className="taboutMe_desciption-text"> The concern to develop and learn new things has always directed my decisions and led me to move around Europe looking for new things and knowledge. I have a psichology degree, and I like to study, as my mind is always active and receptive to new knowledge, I decidee to move on the technological world where I see a great opportunity for growth. I really like meet new challenges and solve problems.</p>
+            <p className="aboutMe_description-phrase"> My brain is happy when is busy.</p>
+        </div>
+    </section>
+
+}
+export default AboutMe;
