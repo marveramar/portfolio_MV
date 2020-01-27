a533ad2d053b4bc870936dec24c950c9642ac331
marvermar <m.veramartin@gmail.com>
2020-01-27 21:07:58 +0100

    finish welcome component, and add all the styles and animations

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,14 @@ const Welcome = () => {
     return (
         <section className="welcome">
             <h1 className="welcome_title">Hello.</h1>
-            <h2 className="welcome_name"> I'm María, front-end developer. </h2>
-            <p className="welcome_text"> My mind ,that has thirst for knowledge, is always active looking for new challenges and ways to see the world</p>
+            <div className="welcome_presentation">
+                <h2 className="welcome_name"> I'm María, front-end developer. </h2>
+                <p className="welcome_text"> My mind ,that has thirst for knowledge, is always active looking for new challenges and ways to see the world</p>
+            </div>
+            <div className="down">
+                <p className="down_more">Go to know more about me </p>
+                <i class="fas fa-chevron-down"></i>
+            </div>
         </section>
     )
 }
