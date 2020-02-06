3459391dda200e7c930565f5902b0bd6a918e768
marvermar <m.veramartin@gmail.com>
2020-02-06 21:56:48 +0100

    finished some styles issues and add link to download cv

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Perfil from '../stylesheets/images/dolly.png';
+import Perfil from '../stylesheets/images/dollybn.jpg';
+import download from '../stylesheets/images/CV_María Vera.zip';
 
 const AboutMe = () => {
     return <section className="aboutMeSection"><a name="aboutMe"></a>
@@ -7,8 +8,10 @@ const AboutMe = () => {
         <div className="aboutMe">
             <img className="aboutMe_img" src={Perfil} alt="fotografía de la autora" />
             <div className="aboutMe_description">
-                <p className="aboutMe_desciption-text"> The concern to develop and learn new things has always directed my decisions and led me to move around Europe looking for new things and knowledge. I have a psichology degree, and I like to study. As my mind is always active and receptive to new knowledge, I decided to move on the technological world where I saw a great growth opportunity. I really like meet new challenges and solve problems.</p>
+                <p className="aboutMe_desciption-text"> The concern to develop and learn new things has always directed my decisions and led me to move around Europe looking for new things and knowledge. I'm also a mountain lover, I like feel nature around that makes me feel balanced. And I'm a dancer, I Love rehearsing once and again. I have a psychology degree,but when I started to work I did realise that was not my world. As my mind is always active and receptive to new knowledge, I decided to move through the technological world where I saw a great growth opportunity. I really like to meet new challenges and solve problems.
+            </p>
                 <p className="aboutMe_description-phrase"> My brain is happy when is busy.</p>
+                <a className="aboutMe_description-download" href={download} download={download}>~Download my CV~</a>
             </div>
         </div>
     </section>
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,50 +1,49 @@
 import React from 'react';
-import ProyectOne from '../stylesheets/images/proyOne.png';
-import ProyectTwo from '../stylesheets/images/proyTwo.png';
-import ProyectThree from '../stylesheets/images/proyThree.png';
-import FinalOne from '../stylesheets/images/finalOne.png';
-import FinalTwo from '../stylesheets/images/finalTwo.png';
-import FinalThree from '../stylesheets/images/finalThree.png';
-import SW from '../stylesheets/images/SW.png';
+import ProyectOne from '../stylesheets/images/P1.png';
+import ProyectTwo from '../stylesheets/images/P2.png';
+import ProyectThree from '../stylesheets/images/P3.png';
+import FinalThree from '../stylesheets/images/R&M.png';
+
 
 const Works = () => {
     return <section className="works"><a name="works"></a>
         <div className="works_description">
             <h4 className="works_title">Works</h4>
-            <p className="works_languages"> Languages I speak:</p>
-            <p className="works_LanguagesSpecific">HTML5, CSS, Sass, JavaScript (Framework React)</p>
-            <h5 className="works_subtitle"> ·· Here, you can find more about my dev jobs. I encorauge you to explore them  and feel free to give me a feedback ·· </h5>
+            <p className="works_languages"> Languages I 'speak':</p>
+            <div className="works_LanguagesSpecific">
+                <i class="fab fa-html5"></i>
+                <i class="fab fa-css3-alt"></i>
+                <i class="fab fa-sass"></i>
+                <i class="fab fa-bootstrap"></i>
+                <i class="fab fa-gulp"></i>
+                <i class="fab fa-github-alt"></i>
+                <i class="fab fa-git-alt"></i>
+            </div>
+            <h5 className="works_subtitle"> ·· Here, you can find more about my dev jobs. I encourage you to explore them  and feel free to give me a feedback ·· </h5>
         </div>
         <div>
             <ul className="works_list">
                 <li>
                     <p className="work_list-title">Front-Walkers</p>
+                    <p className="work_list-description">Creation of a website with two pages, one for the presentation of the creative team and the other with a contact form. This was my first group project in the world of web development. We use HTML5, CSS3, Sass, Bootstrap and git for version control.</p>
                     <a href="https://github.com/marveramar/project-promo-h-module-1-team-2-afternoon-sprint-2-" target="blank"><img className="work_list-img" src={ProyectOne} alt="link P1" /> </a>
                 </li>
                 <li>
                     <p className="work_list-title">Coco-Lab</p>
+                    <p className="work_list-description">Development of a dynamic website to create personalized contact cards. Use of JavaScript to create several functionalities. Developed in a work team with Agile philosophy.</p>
                     <a href="https://github.com/marveramar/project-promo-h-module-2-team-4-afternoon?organization=marveramar&organization=marveramar" target="blank"><img className="work_list-img" src={ProyectTwo} alt="link P2" /></a>
                 </li>
                 <li>
                     <p className="work_list-title">Las Merches</p>
+                    <p className="work_list-description">Migration of legacy code from JavaScript to React. Some new functionality had to be added and the style changed at some point. The developer team worked using Scrum work process.</p>
                     <a href="https://github.com/marveramar/project-promo-h-module-3-team-4-afternoon-" target="blank"><img className="work_list-img" src={ProyectThree} alt="link P3" /></a>
                 </li>
                 <li>
-                    <p className="work_list-title">Layout</p>
-                    <a href="https://github.com/marveramar/evaluacion-final-modulo-1-marveramar" target="blank"><img className="work_list-img" src={FinalOne} alt="finalOne" /></a>
-                </li>
-                <li>
-                    <p className="work_list-title">Film search</p>
-                    <a href="https://github.com/marveramar/modulo-2-evaluacion-final-marveramar" target="blank"><img className="work_list-img" src={FinalTwo} alt="finalTwo" /></a>
-                </li>
-                <li>
                     <p className="work_list-title">Rick&Morty</p>
+                    <p className="work_list-description">Development of web search for characters from a cartoon series. It is a SPA application implemented in React, using React router.</p>
                     <a href="https://github.com/marveramar/modulo-3-evaluacion-final-marveramar" target="blank"><img className="work_list-img" src={FinalThree} alt="finalThree" /></a>
                 </li>
-                <li>
-                    <p className="work_list-title">Random</p>
-                    <a href="https://github.com/marveramar/random-things-1" target="blank"><img className="work_list-img" src={SW} alt="random" /></a>
-                </li>
+
             </ul>
         </div>
     </section>
